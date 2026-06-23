4576bda7d333035cb123028619b671dfb2d79200
shubhzVidhate <shubhamvidhate302@gmail.com>
2026-06-24 00:10:16 +0530

    Done Calculator Project

--- ReactProject_With_JS/src/App.jsx
+++ ReactProject_With_JS/src/App.jsx
@@ -3,13 +3,15 @@ import React from 'react'
 // import Counter from './Components/Counter'
 import Todo from './Components/Todo';
 import Meals from './Components/Meals';
+import Calculator from './Components/Calculator';
 
 const App = () => {
   return (
 
     // <Counter />
     // <Todo />
-    <Meals />
+    // <Meals />
+    <Calculator />
 
   )
 };
