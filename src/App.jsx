e150a180f437efb1ab5c73f79e341347ecc68afe
shubhzVidhate <shubhamvidhate302@gmail.com>
2026-06-23 17:15:33 +0530

    useRef example done

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,42 @@
-
 // import UpdateUserContext from './Components/UseContext/UpdateUserContext.jsx'
 // import { ContextProvider } from './Components/UseContext/UserContext.jsx'
 // import UserProfile from './Components/UseContext/UserProfile.jsx'
 // import ReducerExample from './Components/useReducer/ReducerExample.jsx'
 // import ReducerExample2 from './Components/useReducer/ReducerExample2.jsx'
+// import Counter from "./Components/useReducer/Counter.jsx"
 
-import Counter from "./Components/useReducer/Counter.jsx"
+// import { useRef } from "react";
+import FocusInput from "./Components/UseRefHook/FocusInput";
+import Timer from "./Components/UseRefHook/Timer";
 
 function App() {
 
+  // const inputElement = useRef(null);
+
+  // console.log(inputElement);
+
+  // const handleClick = ()=>{
+  //   inputElement.current.focus();
+  //   inputElement.current.value = "Shubham"
+  // }
+
   return (
-  //  <ContextProvider>
-  //     <UserProfile />
-  //     <UpdateUserContext />
-  //  </ContextProvider>
-  // <ReducerExample />
-  // <ReducerExample2 />
-    <Counter />
-  )
+    //  <ContextProvider>
+    //     <UserProfile />
+    //     <UpdateUserContext />
+    //  </ContextProvider>
+    // <ReducerExample />
+    // <ReducerExample2 />
+    // <Counter />
+
+    // <div>
+    //   <input type="text" ref={inputElement}></input>
+    //   <button onClick={handleClick}>Add Focus</button>
+    // </div>
+
+    // <FocusInput/>
+    // <Timer />
+  );
 }
 
-export default App
+export default App;
